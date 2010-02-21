64dd6c8d6a0e7d9e603361a5574ba6c2b0fcaef7
Martin Králik <majak47@gmail.com>
2010-02-21 22:21:47 +0000

    Sformatovane XML posielane AISu na viacero riadkov. Vsimol som si ze su skoro totozne. Ak takato situacia nastane aj pri dalsich requestoch, tak sa to oplati dat niekam inam a zo spolocneho zakladu generovat.
    git-svn-id: https://fajr.googlecode.com/svn/trunk@14 e29e1642-1b52-11df-8da0-9131547f57cf

--- AIS2AdministraciaStudiaScreen.php
+++ AIS2AdministraciaStudiaScreen.php
@@ -27,95 +27,184 @@ Copyright (c) 2010 Martin Králik
 require_once 'AIS2AbstractScreen.php';
 require_once 'Table.php';
 
-	/**
-	 * Trieda reprezentujúca jednu obrazovku so zoznamom štúdií a zápisných listov.
-	 *
-	 * @author majak
-	 */
-	class AIS2AdministraciaStudiaScreen extends AIS2AbstractScreen
+/**
+ * Trieda reprezentujúca jednu obrazovku so zoznamom štúdií a zápisných listov.
+ *
+ * @author majak
+ */
+class AIS2AdministraciaStudiaScreen extends AIS2AbstractScreen
+{
+	protected $tabulka_zoznam_studii = array(
+		array('name' => 'rocnik',              'title' => 'ročník',               'order' => '0'),
+		array('name' => 'skratka',             'title' => 'skratka',              'order' => '0'),
+		array('name' => 'kruzok',              'title' => 'krúžok',               'order' => '0'),
+		array('name' => 'studijnyProgram',     'title' => 'študijný program',     'order' => '0'),
+		array('name' => 'doplnujuceUdaje',     'title' => 'doplňujúce údaje',     'order' => '0'),
+		array('name' => 'zaciatokStudia',      'title' => 'začiatok štúdia',      'order' => '0'),
+		array('name' => 'koniecStudia',        'title' => 'koniec štúdia',        'order' => '0'),
+		array('name' => 'dlzkaVSemestroch',    'title' => 'dĺžka v semestroch',   'order' => '0'),
+		array('name' => 'dlzkaStudia',         'title' => 'dĺžka štúdia',         'order' => '0'),
+		array('name' => 'cisloDiplomu',        'title' => 'číslo diplomu',        'order' => '0'),
+		array('name' => 'cisloZMatriky',       'title' => 'číslo z matriky',      'order' => '0'),
+		array('name' => 'cisloVysvedcenia',    'title' => 'číslo vysvedčenia',    'order' => '0'),
+		array('name' => 'cisloDodatku',        'title' => 'číslo dodatku',        'order' => '0'),
+		array('name' => 'cisloEVI',            'title' => 'číslo EVI',            'order' => '0'),
+		array('name' => 'cisloProgramu',       'title' => 'číslo programu',       'order' => '0'),
+		array('name' => 'priznak',             'title' => 'príznak',              'order' => '0'),
+		array('name' => 'organizacnaJednotka', 'title' => 'organizačná jednotka', 'order' => '0'),
+		array('name' => 'rokStudia',           'title' => 'rok štúdia',           'order' => '0'),
+	);
+	protected $tabulka_zoznam_zapisnych_listov = array(
+		array('name' => 'akademickyRok',          'title' => 'akademický rok',           'order' => '0'),
+		array('name' => 'rocnik',                 'title' => 'ročník',                   'order' => '0'),
+		array('name' => 'studProgramSkratka',     'title' => 'krúžok',                   'order' => '0'),
+		array('name' => 'studijnyProgram',        'title' => 'skratka',                  'order' => '0'),
+		array('name' => 'doplnujuceUdaje',        'title' => 'doplňujúce údaje',         'order' => '0'),
+		array('name' => 'datumZapisu',            'title' => 'dátum zápisu',             'order' => '0'),
+		array('name' => 'potvrdenyZapis',         'title' => 'potvrdený zápis',          'order' => '0'),
+		array('name' => 'podmienecnyZapis',       'title' => 'podmienečný zápis',        'order' => '0'),
+		array('name' => 'dlzkaVSemestroch',       'title' => 'dĺžka v semestroch',       'order' => '0'),
+		array('name' => 'cisloEVI',               'title' => 'číslo EVI',                'order' => '0'),
+		array('name' => 'cisloProgramu',          'title' => 'číslo programu',           'order' => '0'),
+		array('name' => 'datumSplnenia',          'title' => 'dátum splnenia',           'order' => '0'),
+		array('name' => 'priznak',                'title' => 'príznak',                  'order' => '0'),
+		array('name' => 'organizacnaJednotka',    'title' => 'organizačná jednotka',     'order' => '0'),
+		array('name' => 'typFinacovania',         'title' => 'typ financovania',         'order' => '0'),
+		array('name' => 'skratkaTypuFinacovania', 'title' => 'skratka typu finacovania', 'order' => '0'),
+	);
+
+	protected $idCache = array();
+
+	public function __construct()
 	{
-		protected $tabulka_zoznam_studii = array(
-			array('name' => 'rocnik',              'title' => 'ročník',               'order' => '0'),
-			array('name' => 'skratka',             'title' => 'skratka',              'order' => '0'),
-			array('name' => 'kruzok',              'title' => 'krúžok',               'order' => '0'),
-			array('name' => 'studijnyProgram',     'title' => 'študijný program',     'order' => '0'),
-			array('name' => 'doplnujuceUdaje',     'title' => 'doplňujúce údaje',     'order' => '0'),
-			array('name' => 'zaciatokStudia',      'title' => 'začiatok štúdia',      'order' => '0'),
-			array('name' => 'koniecStudia',        'title' => 'koniec štúdia',        'order' => '0'),
-			array('name' => 'dlzkaVSemestroch',    'title' => 'dĺžka v semestroch',   'order' => '0'),
-			array('name' => 'dlzkaStudia',         'title' => 'dĺžka štúdia',         'order' => '0'),
-			array('name' => 'cisloDiplomu',        'title' => 'číslo diplomu',        'order' => '0'),
-			array('name' => 'cisloZMatriky',       'title' => 'číslo z matriky',      'order' => '0'),
-			array('name' => 'cisloVysvedcenia',    'title' => 'číslo vysvedčenia',    'order' => '0'),
-			array('name' => 'cisloDodatku',        'title' => 'číslo dodatku',        'order' => '0'),
-			array('name' => 'cisloEVI',            'title' => 'číslo EVI',            'order' => '0'),
-			array('name' => 'cisloProgramu',       'title' => 'číslo programu',       'order' => '0'),
-			array('name' => 'priznak',             'title' => 'príznak',              'order' => '0'),
-			array('name' => 'organizacnaJednotka', 'title' => 'organizačná jednotka', 'order' => '0'),
-			array('name' => 'rokStudia',           'title' => 'rok štúdia',           'order' => '0'),
-		);
-		protected $tabulka_zoznam_zapisnych_listov = array(
-			array('name' => 'akademickyRok',          'title' => 'akademický rok',           'order' => '0'),
-			array('name' => 'rocnik',                 'title' => 'ročník',                   'order' => '0'),
-			array('name' => 'studProgramSkratka',     'title' => 'krúžok',                   'order' => '0'),
-			array('name' => 'studijnyProgram',        'title' => 'skratka',                  'order' => '0'),
-			array('name' => 'doplnujuceUdaje',        'title' => 'doplňujúce údaje',         'order' => '0'),
-			array('name' => 'datumZapisu',            'title' => 'dátum zápisu',             'order' => '0'),
-			array('name' => 'potvrdenyZapis',         'title' => 'potvrdený zápis',          'order' => '0'),
-			array('name' => 'podmienecnyZapis',       'title' => 'podmienečný zápis',        'order' => '0'),
-			array('name' => 'dlzkaVSemestroch',       'title' => 'dĺžka v semestroch',       'order' => '0'),
-			array('name' => 'cisloEVI',               'title' => 'číslo EVI',                'order' => '0'),
-			array('name' => 'cisloProgramu',          'title' => 'číslo programu',           'order' => '0'),
-			array('name' => 'datumSplnenia',          'title' => 'dátum splnenia',           'order' => '0'),
-			array('name' => 'priznak',                'title' => 'príznak',                  'order' => '0'),
-			array('name' => 'organizacnaJednotka',    'title' => 'organizačná jednotka',     'order' => '0'),
-			array('name' => 'typFinacovania',         'title' => 'typ financovania',         'order' => '0'),
-			array('name' => 'skratkaTypuFinacovania', 'title' => 'skratka typu finacovania', 'order' => '0'),
-		);
-
-		protected $idCache = array();
-
-		public function __construct()
-		{
-			parent::__construct('ais.gui.vs.es.VSES017App', '&kodAplikacie=VSES017');
-		}
-
-		public function getZoznamStudii()
-		{
-			$data = pluck($this->data, AIS2Utils::DATA_PATTERN);
-			return new Table($this->tabulka_zoznam_studii, $data, 'Zoznam štúdií', 'studium');
-		}
-
-		public function getZapisneListy($studiumIndex)
-		{
-			$data = AIS2Utils::request($this->getXmlInterfaceLocation(), array('xml_spec' => '<request> <serial>'.$this->getSerial().'</serial> <events> <ev> <dlgName>VSES017_StudentZapisneListyDlg0</dlgName> <compName>nacitatDataAction</compName> <event class=\'avc.ui.event.AVCActionEvent\'></event> </ev> </events> <changedProps> <changedProperties><objName>app</objName><propertyValues> <nameValue><name>activeDlgName</name><value>VSES017_StudentZapisneListyDlg0</value></nameValue> </propertyValues></changedProperties> <changedProperties><objName>VSES017_StudentZapisneListyDlg0</objName> <propertyValues> <nameValue> <name>x</name> <value>-4</value> </nameValue> <nameValue> <name>y</name> <value>-4</value> </nameValue> <nameValue> <name>focusedComponent</name> <value>nacitatButton</value> </nameValue> </propertyValues> <embObjChProps> <changedProperties> <objName>studiaTable</objName> <propertyValues> <nameValue>  <name>dataView</name>  <isXml>true</isXml>  <value><![CDATA[  <root>  <selection>  <activeIndex>'.$studiumIndex.'</activeIndex>  <selectedIndexes>'.$studiumIndex.'</selectedIndexes>  </selection>  </root>  ]]></value> </nameValue><nameValue> <name>editMode</name>  <isXml>false</isXml>  <value>false</value></nameValue></propertyValues> <embObjChProps isNull=\'true\'/> </changedProperties> </embObjChProps> </changedProperties> </changedProps> </request>'));
-			$data = pluck($data, AIS2Utils::DATA_PATTERN);
-			return new Table($this->tabulka_zoznam_zapisnych_listov, $data, 'Zoznam zápisných listov', 'list', array('studium' => $studiumIndex));
-		}
-
-		public function getIdZapisnyList($zapisnyListIndex)
-		{
-			return $this->getIdFromZapisnyListIndex($zapisnyListIndex, 'idZapisnyList');
-		}
-
-		public function getIdStudium($zapisnyListIndex)
-		{
-			return $this->getIdFromZapisnyListIndex($zapisnyListIndex, 'idStudium');
-		}
-
-		protected function getIdFromZapisnyListIndex($zapisnyListIndex, $idType)
-		{
-			if (empty($this->idCache[$zapisnyListIndex]))
-			{
-				$data = AIS2Utils::request($this->getXmlInterfaceLocation(), array('xml_spec' => '<request><serial>'.$this->getSerial().'</serial><events><ev><dlgName>VSES017_StudentZapisneListyDlg0</dlgName><compName>terminyHodnoteniaAction</compName><event class=\'avc.ui.event.AVCActionEvent\'></event></ev></events><changedProps><changedProperties><objName>app</objName><propertyValues><nameValue><name>activeDlgName</name><value>VSES017_StudentZapisneListyDlg0</value></nameValue></propertyValues></changedProperties><changedProperties><objName>VSES017_StudentZapisneListyDlg0</objName><propertyValues><nameValue><name>x</name><value>-4</value></nameValue><nameValue><name>y</name><value>-4</value></nameValue><nameValue><name>focusedComponent</name><value>zapisneListyTable</value></nameValue></propertyValues><embObjChProps><changedProperties><objName>zapisneListyTable</objName><propertyValues><nameValue><name>dataView</name><isXml>true</isXml><value><![CDATA[<root><selection><activeIndex>'.$zapisnyListIndex.'</activeIndex><selectedIndexes>'.$zapisnyListIndex.'</selectedIndexes></selection></root>]]></value></nameValue><nameValue><name>editMode</name><isXml>false</isXml><value>false</value></nameValue></propertyValues><embObjChProps isNull=\'true\'/></changedProperties></embObjChProps></changedProperties></changedProps></request>'));
-				$data = pluckAll($data, AIS2Utils::APP_LOCATION_PATTERN, true);
-				$data = pluckAll($data[2], '@&idZapisnyList\=(?P<idZapisnyList>[0-9]*)&idStudium\=(?P<idStudium>[0-9]*)@', true);
-				foreach (array_keys($data) as $key) if (is_numeric($key)) unset($data[$key]);
-				$this->idCache[$zapisnyListIndex] = $data;
-			}
-			return $this->idCache[$zapisnyListIndex][$idType];
-		}
-
+		parent::__construct('ais.gui.vs.es.VSES017App', '&kodAplikacie=VSES017');
 	}
-	
+
+	public function getZoznamStudii()
+	{
+		$data = pluck($this->data, AIS2Utils::DATA_PATTERN);
+		return new Table($this->tabulka_zoznam_studii, $data, 'Zoznam štúdií', 'studium');
+	}
+
+	public function getZapisneListy($studiumIndex)
+	{
+		$data = AIS2Utils::request($this->getXmlInterfaceLocation(), array('xml_spec' => '
+<request>
+	<serial>'.$this->getSerial().'</serial>
+	<events><ev>
+		<dlgName>VSES017_StudentZapisneListyDlg0</dlgName>
+		<compName>nacitatDataAction</compName>
+		<event class=\'avc.ui.event.AVCActionEvent\'></event>
+	</ev></events>
+	<changedProps>
+		<changedProperties>
+			<objName>app</objName>
+			<propertyValues><nameValue>
+				<name>activeDlgName</name><value>VSES017_StudentZapisneListyDlg0</value>
+			</nameValue></propertyValues>
+		</changedProperties>
+		<changedProperties>
+			<objName>VSES017_StudentZapisneListyDlg0</objName>
+			<propertyValues>
+				<nameValue><name>x</name><value>-4</value></nameValue>
+				<nameValue><name>y</name><value>-4</value></nameValue>
+				<nameValue><name>focusedComponent</name><value>nacitatButton</value></nameValue>
+			</propertyValues>
+			<embObjChProps><changedProperties>
+				<objName>studiaTable</objName>
+				<propertyValues>
+					<nameValue>
+						<name>dataView</name>
+						<isXml>true</isXml>
+						<value><![CDATA[
+							<root><selection>
+								<activeIndex>'.$studiumIndex.'</activeIndex>
+								<selectedIndexes>'.$studiumIndex.'</selectedIndexes>
+							</selection></root>
+						]]></value>
+					</nameValue>
+					<nameValue>
+						<name>editMode</name>
+						<isXml>false</isXml>
+						<value>false</value>
+					</nameValue>
+				</propertyValues>
+				<embObjChProps isNull=\'true\'/>
+			</changedProperties></embObjChProps>
+		</changedProperties>
+	</changedProps>
+</request>'));
+		$data = pluck($data, AIS2Utils::DATA_PATTERN);
+		return new Table($this->tabulka_zoznam_zapisnych_listov, $data, 'Zoznam zápisných listov', 'list', array('studium' => $studiumIndex));
+	}
+
+	public function getIdZapisnyList($zapisnyListIndex)
+	{
+		return $this->getIdFromZapisnyListIndex($zapisnyListIndex, 'idZapisnyList');
+	}
+
+	public function getIdStudium($zapisnyListIndex)
+	{
+		return $this->getIdFromZapisnyListIndex($zapisnyListIndex, 'idStudium');
+	}
+
+	protected function getIdFromZapisnyListIndex($zapisnyListIndex, $idType)
+	{
+		if (empty($this->idCache[$zapisnyListIndex]))
+		{
+			$data = AIS2Utils::request($this->getXmlInterfaceLocation(), array('xml_spec' => '
+<request>
+	<serial>'.$this->getSerial().'</serial>
+	<events><ev>
+		<dlgName>VSES017_StudentZapisneListyDlg0</dlgName>
+		<compName>terminyHodnoteniaAction</compName>
+		<event class=\'avc.ui.event.AVCActionEvent\'></event>
+	</ev></events>
+	<changedProps>
+		<changedProperties>
+			<objName>app</objName>
+			<propertyValues><nameValue>
+				<name>activeDlgName</name><value>VSES017_StudentZapisneListyDlg0</value>
+			</nameValue></propertyValues>
+		</changedProperties>
+		<changedProperties>
+			<objName>VSES017_StudentZapisneListyDlg0</objName>
+			<propertyValues>
+				<nameValue><name>x</name><value>-4</value></nameValue>
+				<nameValue><name>y</name><value>-4</value></nameValue>
+				<nameValue><name>focusedComponent</name><value>zapisneListyTable</value></nameValue>
+			</propertyValues>
+			<embObjChProps><changedProperties>
+				<objName>zapisneListyTable</objName>
+				<propertyValues>
+					<nameValue>
+						<name>dataView</name>
+						<isXml>true</isXml>
+						<value><![CDATA[
+							<root><selection>
+								<activeIndex>'.$zapisnyListIndex.'</activeIndex>
+								<selectedIndexes>'.$zapisnyListIndex.'</selectedIndexes>
+							</selection></root>
+						]]></value>
+					</nameValue>
+					<nameValue>
+						<name>editMode</name>
+						<isXml>false</isXml>
+						<value>false</value>
+					</nameValue>
+				</propertyValues>
+				<embObjChProps isNull=\'true\'/>
+			</changedProperties></embObjChProps>
+		</changedProperties>
+	</changedProps>
+</request>'));
+			$data = pluckAll($data, AIS2Utils::APP_LOCATION_PATTERN, true);
+			$data = pluckAll($data[2], '@&idZapisnyList\=(?P<idZapisnyList>[0-9]*)&idStudium\=(?P<idStudium>[0-9]*)@', true);
+			foreach (array_keys($data) as $key) if (is_numeric($key)) unset($data[$key]);
+			$this->idCache[$zapisnyListIndex] = $data;
+		}
+		return $this->idCache[$zapisnyListIndex][$idType];
+	}
+
+}
 ?>
