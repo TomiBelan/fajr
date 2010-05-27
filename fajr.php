106ec0641136e7b64ccdfbd1f06733cd1c89c0f8
Peter Perešíni <ppershing@gmail.com>
2010-05-27 19:51:50 +0000

    Default timezone
    git-svn-id: https://fajr.googlecode.com/svn/trunk@75 e29e1642-1b52-11df-8da0-9131547f57cf

--- fajr.php
+++ fajr.php
@@ -25,6 +25,7 @@ Copyright (c) 2010 Martin Králik
  }}} */
 
 error_reporting(E_ALL | E_STRICT);
+date_default_timezone_set('Europe/Bratislava');
 
 // Pretoze v session ukladam objekty libfajru, treba nacitat definicie
 // tried skor, ako sa nacitava session
