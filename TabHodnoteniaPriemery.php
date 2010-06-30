8e69aa8312cde805d5bf11ccfe5dd847c887d050
Martin Sucha <anty.sk@gmail.com>
2010-06-30 23:36:20 +0000

    Pridane zakladne pocitanie studijnych priemerov do fajru
    git-svn-id: https://fajr.googlecode.com/svn/trunk@160 e29e1642-1b52-11df-8da0-9131547f57cf

--- TabHodnoteniaPriemery.php
+++ TabHodnoteniaPriemery.php
@@ -1,5 +1,7 @@
 <?php
 require_once 'Renderable.php';
+require_once 'Container.php';
+require_once 'Label.php';
 require_once 'Table.php';
 require_once 'libfajr/AIS2Utils.php';
 require_once 'libfajr/AIS2AdministraciaStudiaScreen.php';
@@ -19,9 +21,35 @@ class HodnoteniaCallback implements Renderable {
 	public function getHtml() {
 		$hodnotenia = $this->app->getHodnotenia();
 		$hodnoteniaTable = new Table(TableDefinitions::hodnotenia());
+
+		$sucet = array('leto'=>0.0, 'zima'=>0.0);
+		$pocet = array('leto'=>0.0, 'zima'=>0.0);
+		$sucetKreditov = array('leto'=>0, 'zima'=>0);
+		$vsetky = array('leto'=>true, 'zima'=>true);
+
+		$numerickaHodnotaZnamky = array('A'=>1.0,
+										'B'=>1.5,
+										'C'=>2.0,
+										'D'=>2.5,
+										'E'=>3.0,
+										'Fx'=>4.0);
+
 		foreach(Sorter::sort($hodnotenia->getData(),
 					array("semester"=>-1, "nazov"=>1)) as $row) {
-			if ($row['semester']=='L') $class='leto'; else $class='zima';
+			if ($row['semester']=='L') {
+				$class='leto';
+			}
+			else {
+				$class='zima';
+			}
+			if (isset($row['znamka']) && !empty($row['znamka'])) {
+				$pocet[$class] += 1;
+				$sucet[$class] += $numerickaHodnotaZnamky[$row['znamka']]*$row['kredit'];
+				$sucetKreditov[$class] += $row['kredit'];
+			}
+			else {
+				$vsetky[$class] = false;
+			}
 			$hodnoteniaTable->addRow($row, array('class'=>$class));
 		}
 
@@ -31,8 +59,35 @@ class HodnoteniaCallback implements Renderable {
 		$priemeryTable = new Table(TableDefinitions::priemery());
 		$priemeryTable->addRows($priemery->getData());
 
-		$priemeryCollapsible = new Collapsible('Priemery ((ne)funguje presne tak ako v AISe, sťažujte sa tam)',
-			$priemeryTable);
+		$priemeryContainer = new Container();
+		$priemeryContainer->addChild(new Label('Nasledovné priemery sú prebraté z AISu, čiže to (ne)funguje presne rovnako:'));
+		$priemeryContainer->addChild($priemeryTable);
+
+		if ($pocet['zima'] > 0 || $pocet['leto'] > 0) {
+			$priemeryFajrText = '<p><br />Nasledovné vážené študijné priemery sú počítané Fajrom z tabuľky Hodnotenia <strong>zo zatiaľ ohodnotených predmetov</strong>:<br />';
+
+			if ($pocet['zima']>0) {
+				$priemerZima = $sucet['zima']/$sucetKreditov['zima'];
+				$priemeryFajrText .= 'Zimný semester: '.sprintf('%.2f', $priemerZima).'<br />';
+			}
+
+			if ($pocet['leto']>0) {
+				$priemerLeto = $sucet['leto']/$sucetKreditov['leto'];
+				$priemeryFajrText .= 'Letný semester: '.sprintf('%.2f', $priemerLeto).'<br />';
+			}
+
+			if ($pocet['zima']>0 && $pocet['leto']>0) {
+				$priemerRok = ($sucet['zima']+$sucet['leto'])/($sucetKreditov['zima']+$sucetKreditov['leto']);
+				$priemeryFajrText .= 'Celý akad. rok: '.sprintf('%.2f', $priemerRok).'<br />';
+			}
+
+
+			$priemeryContainer->addChild(new Label($priemeryFajrText));
+			$priemeryFajrText .= '</p>';
+		}
+		
+
+		$priemeryCollapsible = new Collapsible('Priemery', $priemeryContainer);
 		
 		return $hodnoteniaCollapsible->getHtml().$priemeryCollapsible->getHtml();
 	}
