78db02b946109006016d89e18337712e73cd8483
Peter Perešíni <ppershing@gmail.com>
2010-07-10 09:54:34 +0000

    Trochu ukecanejsie generovanie grafov
    git-svn-id: https://fajr.googlecode.com/svn/trunk@172 e29e1642-1b52-11df-8da0-9131547f57cf

--- scripts/make_all_doc.sh
+++ scripts/make_all_doc.sh
@@ -5,6 +5,9 @@ SCRIPT_GRAPH_PACKAGE="phpdoc/package_graph.sh";
 
 BASE_PATH=`dirname $0`/..
 $SCRIPT $BASE_PATH "Fajr";
+echo "Generating class graph"
 $SCRIPT_GRAPH_CLASS $BASE_PATH/report/documentation $BASE_PATH/report/
+echo "Generating dir graph"
 $SCRIPT_GRAPH_DIR $BASE_PATH
+echo "Generating package graph"
 $SCRIPT_GRAPH_PACKAGE $BASE_PATH
