23b92b6754f257ddfb81e0eaa750a629da7136be
Peter Perešíni <ppershing@gmail.com>
2010-07-10 09:12:03 +0000

    Pridany coding standard pre phpcs a trochu pofixovane skripty.
    git-svn-id: https://fajr.googlecode.com/svn/trunk@169 e29e1642-1b52-11df-8da0-9131547f57cf

--- scripts/make_all_doc.sh
+++ scripts/make_all_doc.sh
@@ -4,7 +4,7 @@ SCRIPT_GRAPH_DIR="phpdoc/directory_graph.sh";
 SCRIPT_GRAPH_PACKAGE="phpdoc/package_graph.sh";
 
 BASE_PATH=`dirname $0`/..
-$SCRIPT $BASE_PATH "FKS Databaza";
+$SCRIPT $BASE_PATH "Fajr";
 $SCRIPT_GRAPH_CLASS $BASE_PATH/report/documentation $BASE_PATH/report/
 $SCRIPT_GRAPH_DIR $BASE_PATH
 $SCRIPT_GRAPH_PACKAGE $BASE_PATH
--- scripts/make_coding_standard.sh
+++ scripts/make_coding_standard.sh
@@ -11,11 +11,10 @@ if [ $? -eq 0 ] ; then
     echo -n "Built on " >> $FILE;
     date >> $FILE;
     echo -e "\n" >> $FILE;
-    phpcs --standard=FKS . --report=full --ignore=report | tr '<>' '()' >> $FILE;
+    phpcs --standard=Fajr . --report=full --ignore=report | tr '<>' '()' >> $FILE;
     echo -e "\n\n\n++++++++++++++++++++++++++++++++++++++++++++++++++\n\n\n" >> $FILE;
-    phpcs --standard=FKS . --report=summary --ignore=report | tr '<>' '()' >> $FILE;
+    phpcs --standard=Fajr . --report=summary --ignore=report | tr '<>' '()' >> $FILE;
     echo "</pre></html>" >> $FILE;
-  else
+else
     echo "Problems entering directory $BASE, exitting";
-  fi;
-fi
+fi;
