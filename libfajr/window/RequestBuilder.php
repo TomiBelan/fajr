195449d0f998aaf047f23e9e7347cc9c29d91060
Peter Perešíni <ppershing@gmail.com>
2010-08-01 18:18:07 +0000

    Refaktoring. Presunutie kódu z AbstractWindow do vlastnej triedy, ktorá bude využívaná AbstractScreenom a AbstractDialogom.
    git-svn-id: https://fajr.googlecode.com/svn/trunk@207 e29e1642-1b52-11df-8da0-9131547f57cf

---
+++ libfajr/window/RequestBuilder.php
@@ -0,0 +1,50 @@
+<?php
+/* {{{
+Copyright (c) 2010 Peter Peresini
+
+ Permission is hereby granted, free of charge, to any person
+ obtaining a copy of this software and associated documentation
+ files (the "Software"), to deal in the Software without
+ restriction, including without limitation the rights to use,
+ copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following
+ conditions:
+
+ The above copyright notice and this permission notice shall be
+ included in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ OTHER DEALINGS IN THE SOFTWARE.
+ }}} */
+namespace AIS2;
+
+interface RequestBuilder {
+  /**
+   * Vygeneruje dáta na POST request
+   * @param string $dlgName názov aktuálneho dialógu
+   * @param array() $options
+   * @return array() POST data array
+   */
+  public function buildRequestData($dlgName, $options);
+
+  /**
+   * Vygeneruje url na ktorú treba robiť request
+   * @param string $appId id AIS aplikácie
+   * @return string url
+   */
+  public function getRequestUrl($appId);
+
+  /**
+   * S každým requestom je treba posielať nový serial.
+   * @return int aktuálny serial pre AIS.
+   */
+  public function newSerial();
+}
+
---
+++ libfajr/window/RequestBuilderImpl.php
@@ -0,0 +1,107 @@
+<?php
+namespace AIS2;
+
+class RequestBuilderImpl implements RequestBuilder {
+  private $serial = 0;
+
+  /**
+   * Vygeneruje nové sériové číslo používané v XML protokole na komunikáciu s AISom.
+   * @return int Nové seriové číslo v poradí.
+   */
+  public function newSerial()
+  {
+    return $this->serial++;
+  }
+
+  /**
+   * Vytvorí url XML interfacu pre komunikáciu s "aplikáciou" tejto obrazovky.
+   * @param string $appId AIS2 id aplikácie.
+   * @return string Url.
+   */
+  public function getRequestUrl($appId)
+  {
+    return 'https://ais2.uniba.sk/ais/servlets/WebUIServlet?appId='.$appId.'&antiCache='.random().'&viewer=web';
+  }
+
+  /**
+  * Experimentalna funkcia snažiaca sa zovšeobecniť dodatočné requesty jednotlivých AIS aplikácií.
+  * Je veľmi pravdepodobné, že sa bude meniť.
+  * @param string $dlgName názov aktuálneho dialógu
+  * @param array() $options špeciálne nastavenia, viď kód.
+  * @return array() POST dáta.
+  */
+  public function buildRequestData($dlgName, array $options)
+  {
+    $events = true;
+    $eventClass = 'avc.ui.event.AVCActionEvent';
+    $app = true;
+    $command = null;
+    $compName = null;
+    $appProperties = array();
+    $embObj = null;
+    $appProperties = array();
+    $objProperties = array();
+    $changedProperties = null;
+    extract($options, EXTR_IF_EXISTS);
+
+    if (!isset($appProperties['activeDlgName'])) $appProperties['activeDlgName'] = $dlgName;
+
+
+    $xml_spec = '<request><serial>'.$this->newSerial().'</serial>';
+    if ($events === true)
+    {
+      $xml_spec .= '<events><ev><dlgName>'.$dlgName.'</dlgName>';
+      if ($compName !== null) $xml_spec .= '<compName>'.$compName.'</compName>';
+      $xml_spec .= '<event class=\''.$eventClass.'\'>';
+      if ($command !== null) $xml_spec .= '<command>'.$command.'</command>';
+      $xml_spec .= '</event></ev></events>';
+    }
+    $xml_spec .= '<changedProps>';
+    if ($app === true)
+    {
+      $xml_spec .= '<changedProperties><objName>app</objName><propertyValues>';
+      foreach ($appProperties as $name => $value) $xml_spec .= '<nameValue><name>'.$name.'</name><value>'.$value.'</value></nameValue>';
+      $xml_spec .= '</propertyValues></changedProperties>';
+    }
+
+    if ($dlgName !== false)
+    {
+      $xml_spec .= '<changedProperties><objName>'.$dlgName.'</objName><propertyValues>';
+      foreach ($objProperties as $name => $value) $xml_spec .= '<nameValue><name>'.$name.'</name><value>'.$value.'</value></nameValue>';
+      $xml_spec .= '</propertyValues><embObjChProps>';
+
+      if ($embObj !== null)
+      {
+        $xml_spec .= '<changedProperties><objName>'.$embObj['objName'].'</objName><propertyValues><nameValue><name>dataView</name><isXml>true</isXml><value><![CDATA[<root><selection>';
+        if (isset($embObj['dataView']) && is_array($embObj['dataView'])) foreach ($embObj['dataView'] as $name => $value) $xml_spec .= '<'.$name.'>'.$value.'</'.$name.'>';
+        $xml_spec .= '</selection>';
+        if (isset($embObj['visibleBuffers'])) $xml_spec .= '<visibleBuffers>'.$embObj['visibleBuffers'].'</visibleBuffers>';
+        if (isset($embObj['loadedBuffers'])) $xml_spec .= '<loadedBuffers>'.$embObj['loadedBuffers'].'</loadedBuffers>';
+        $xml_spec .= '
+                </root>
+              ]]></value>
+            </nameValue>
+            <nameValue>
+              <name>editMode</name>
+              <isXml>false</isXml>
+              <value>false</value>
+            </nameValue>
+          </propertyValues>
+          <embObjChProps isNull=\'true\'/>
+        </changedProperties>';
+      }
+      $xml_spec .= '</embObjChProps></changedProperties>';
+    }
+    
+    if (is_array($changedProperties)) 
+    {
+      $xml_spec .= '<changedProperties><propertyValues>';
+      foreach ($changedProperties as $name => $value) $xml_spec .= '<nameValue><name>'.$name.'</name><value>'.$value.'</value></nameValue>';
+      $xml_spec .= '</propertyValues></changedProperties>';
+    }
+    
+    $xml_spec .= '</changedProps></request>';
+    
+    return array('xml_spec' => $xml_spec);
+  }
+}
