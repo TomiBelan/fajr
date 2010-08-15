1492ca10929d9164abcdb68213852a61cdcb6a10
Peter Perešíni <ppershing@gmail.com>
2010-08-15 18:40:40 +0000

    Pri testovani chceme vidiet vsetky errory.
    git-svn-id: https://fajr.googlecode.com/svn/branches/ppershing_trace_devel@270 e29e1642-1b52-11df-8da0-9131547f57cf

--- test_include.php
+++ test_include.php
@@ -3,4 +3,5 @@ require_once 'PHPUnit/Framework.php';
 require_once 'libfajr/libfajr.php';
 Loader::register();
 Loader::searchForClasses(__DIR__, true);
+ini_set('error_reporting', E_ALL | E_STRICT);
 ?>
