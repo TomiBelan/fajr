af962f54cabfb6f69694dde36a1aca76ddd6c099
Peter Perešíni <ppershing@gmail.com>
2010-08-15 18:55:37 +0000

    tabs->spaces + indent fajr.php
    git-svn-id: https://fajr.googlecode.com/svn/branches/ppershing_trace_devel@276 e29e1642-1b52-11df-8da0-9131547f57cf

--- fajr.php
+++ fajr.php
@@ -73,176 +73,173 @@ class Fajr {
 
   public function run()
   {
+    $timer = new SystemTimer();
+    $connection = null;
+    $statsConnection = null;
+    $rawStatsConnection = null;
 
-$timer = new SystemTimer();
-$connection = null;
-$statsConnection = null;
-$rawStatsConnection = null;
+    $trace = new HtmlTrace($timer, "--Trace--");
 
-$trace = new HtmlTrace($timer, "--Trace--");
+    try
+    {
+      Input::prepare();
 
-try
-{
-  Input::prepare();
+      $login = Input::get('login'); Input::set('login', null);
+      $krbpwd = Input::get('krbpwd'); Input::set('krbpwd', null);
+      $cosignCookie = Input::get('cosignCookie'); Input::set('cosignCookie', null);
 
-  $login = Input::get('login'); Input::set('login', null);
-	$krbpwd = Input::get('krbpwd'); Input::set('krbpwd', null);
-	$cosignCookie = Input::get('cosignCookie'); Input::set('cosignCookie', null);
+      // FIXME this should be refactored
+      if (($login !== null && $krbpwd !== null) || ($cosignCookie !== null)) {
+        // we are going to log in, so we get a clean session
+        // this needs to be done before a connection
+        // is created, because we pass cookie file name
+        // that contains session_id into AIS2CurlConnection
+        // If we regenerated the session id afterwards,
+        // we could not find the cookie file after a redirect
+        FajrUtils::dropSession();
+      }
 
-  // FIXME this should be refactored
-  if (($login !== null && $krbpwd !== null) || ($cosignCookie !== null)) {
-    // we are going to log in, so we get a clean session
-    // this needs to be done before a connection
-    // is created, because we pass cookie file name
-    // that contains session_id into AIS2CurlConnection
-    // If we regenerated the session id afterwards,
-    // we could not find the cookie file after a redirect
-    FajrUtils::dropSession();
-  }
+      $connection = new connection\CurlConnection(FajrUtils::getCookieFile());
 
-	$connection = new connection\CurlConnection(FajrUtils::getCookieFile());
+      $rawStatsConnection = new connection\StatsConnection($connection, new SystemTimer());
+      $connection = $rawStatsConnection;
 
-	$rawStatsConnection = new connection\StatsConnection($connection, new SystemTimer());
-	$connection = $rawStatsConnection;
+      $connection = new connection\GzipDecompressingConnection($connection, FajrConfig::getDirectory('Path.Temporary'));
+      $connection = new connection\AIS2ErrorCheckingConnection($connection);
 
-	$connection = new connection\GzipDecompressingConnection($connection, FajrConfig::getDirectory('Path.Temporary'));
-	$connection = new connection\AIS2ErrorCheckingConnection($connection);
+      $statsConnection = new connection\StatsConnection($connection, new SystemTimer());
+      $connection = $statsConnection;
+      $simpleConnection = new connection\HttpToSimpleConnectionAdapter($connection);
 
-	$statsConnection = new connection\StatsConnection($connection, new SystemTimer());
-	$connection = $statsConnection;
-  $simpleConnection = new connection\HttpToSimpleConnectionAdapter($connection);
+      AIS2Utils::connection($simpleConnection); // toto tu je docasne
 
-	AIS2Utils::connection($simpleConnection); // toto tu je docasne
+      if (Input::get('logout') !== null) {
+        FajrUtils::logout($connection);
+        FajrUtils::redirect();
+      }
 
-	if (Input::get('logout') !== null) {
-    FajrUtils::logout($connection);
-    FajrUtils::redirect();
-  }
+      if ($login !== null && $krbpwd !== null) {
+        $loggedIn = FajrUtils::login(new AIS2CosignLogin($login, $krbpwd), $connection);
+        $login = null;
+        $krbpwd = null;
+      } else if ($cosignCookie !== null) {
+        $loggedIn = FajrUtils::login(new AIS2CookieLogin($cosignCookie), $connection);
+        $cosignCookie = null;
+      } else {
+        $loggedIn = FajrUtils::isLoggedIn();
+      }
 
-	if ($login !== null && $krbpwd !== null) {
-		$loggedIn = FajrUtils::login(new AIS2CosignLogin($login, $krbpwd), $connection);
-		$login = null;
-		$krbpwd = null;
-	} else if ($cosignCookie !== null) {
-		$loggedIn = FajrUtils::login(new AIS2CookieLogin($cosignCookie), $connection);
-		$cosignCookie = null;
-	} else {
-		$loggedIn = FajrUtils::isLoggedIn();
-	}
+      if ($loggedIn) {
+        DisplayManager::addContent(
+        '<div class=\'logout\'><a class="button negative" href="'.FajrUtils::linkUrl(array('logout'=>true)).'">
+        <img src="images/door_in.png" alt=""/>Odhlásiť</a></div>'
+        );
+        $adminStudia = new AIS2AdministraciaStudiaScreen($trace, $simpleConnection);
+        
+        if (Input::get('studium') === null) Input::set('studium',0);
+        
+        $zoznamStudii = $adminStudia->getZoznamStudii($trace->addChild("Get Zoznam Studii:"));
+        $zoznamStudiiTable = new Table(TableDefinitions::zoznamStudii(), 'studium',
+          array('tab' => Input::get('tab')));
+        $zoznamStudiiTable->addRows($zoznamStudii->getData());
+        $zoznamStudiiTable->setOption('selected_key', Input::get('studium'));
+        $zoznamStudiiTable->setOption('collapsed', true);
 
-	if ($loggedIn) {
-		DisplayManager::addContent(
-		'<div class=\'logout\'><a class="button negative" href="'.FajrUtils::linkUrl(array('logout'=>true)).'">
-	  <img src="images/door_in.png" alt=""/>Odhlásiť</a></div>'
-		);
-		$adminStudia = new AIS2AdministraciaStudiaScreen($trace, $simpleConnection);
-		
-		if (Input::get('studium') === null) Input::set('studium',0);
-		
-		$zoznamStudii = $adminStudia->getZoznamStudii($trace->addChild("Get Zoznam Studii:"));
-		$zoznamStudiiTable = new Table(TableDefinitions::zoznamStudii(), 'studium',
-			array('tab' => Input::get('tab')));
-		$zoznamStudiiTable->addRows($zoznamStudii->getData());
-		$zoznamStudiiTable->setOption('selected_key', Input::get('studium'));
-		$zoznamStudiiTable->setOption('collapsed', true);
+        $zoznamStudiiCollapsible = new Collapsible(new HtmlHeader('Zoznam štúdií'), $zoznamStudiiTable, true);
 
-		$zoznamStudiiCollapsible = new Collapsible(new HtmlHeader('Zoznam štúdií'), $zoznamStudiiTable, true);
+        DisplayManager::addContent($zoznamStudiiCollapsible->getHtml());		
+        
+        $zapisneListy = $adminStudia->getZapisneListy($trace->addChild('getZapisneListy'), Input::get('studium'));
+        
+        $zapisneListyTable = new
+          Table(TableDefinitions::zoznamZapisnychListov(),
+            'list', array('studium' => Input::get('studium'),
+              'tab'=>Input::get('tab')));
+        
+        if (Input::get('list') === null) {
+          $tmp = $zapisneListy->getData();
+          $lastList = end($tmp);
+          Input::set('list', $lastList['index']);
+        }
+        
+        $zapisneListyTable->addRows($zapisneListy->getData());
+        $zapisneListyTable->setOption('selected_key', Input::get('list'));
+        $zapisneListyTable->setOption('collapsed', true);
 
-		DisplayManager::addContent($zoznamStudiiCollapsible->getHtml());		
-		
-		$zapisneListy = $adminStudia->getZapisneListy($trace->addChild('getZapisneListy'), Input::get('studium'));
-		
-		$zapisneListyTable = new
-			Table(TableDefinitions::zoznamZapisnychListov(),
-				'list', array('studium' => Input::get('studium'),
-					'tab'=>Input::get('tab')));
-		
-		if (Input::get('list') === null) {
-			$tmp = $zapisneListy->getData();
-			$lastList = end($tmp);
-			Input::set('list', $lastList['index']);
-		}
-		
-		$zapisneListyTable->addRows($zapisneListy->getData());
-		$zapisneListyTable->setOption('selected_key', Input::get('list'));
-		$zapisneListyTable->setOption('collapsed', true);
+        $zapisneListyCollapsible = new Collapsible(new HtmlHeader('Zoznam zápisných listov'), $zapisneListyTable, true);
 
-		$zapisneListyCollapsible = new Collapsible(new HtmlHeader('Zoznam zápisných listov'), $zapisneListyTable, true);
+        DisplayManager::addContent($zapisneListyCollapsible->getHtml());
+        
+        
+        $terminyHodnotenia = new
+          AIS2TerminyHodnoteniaScreen(
+              $trace,
+              $simpleConnection,
+              $adminStudia->getIdZapisnyList($trace, Input::get('list')),
+              $adminStudia->getIdStudium($trace, Input::get('list')));
+        
+        if (Input::get('tab') === null) Input::set('tab', 'TerminyHodnotenia');
+        $tabs = new TabManager('tab', array('studium'=>Input::get('studium'),
+              'list'=>Input::get('list')));
+        // FIXME: chceme to nejak refaktorovat, aby sme nevytvarali zbytocne
+        // objekty, ktore v konstruktore robia requesty
+        $hodnoteniaScreen = new AIS2HodnoteniaPriemeryScreen(
+              $trace, $simpleConnection,
+              $adminStudia->getIdZapisnyList($trace,
+                Input::get('list')));
+        $tabs->addTab('TerminyHodnotenia', 'Moje skúšky',
+              new MojeTerminyHodnoteniaCallback($trace->addChild("terminy hodnotenia callback"), $terminyHodnotenia, $hodnoteniaScreen));
+        $tabs->addTab('ZapisSkusok', 'Prihlásenie na skúšky',
+              new ZoznamTerminovCallback($terminyHodnotenia, $hodnoteniaScreen));
+        $tabs->addTab('ZapisnyList', 'Zápisný list',
+              new ZapisanePredmetyCallback($terminyHodnotenia));
+        $tabs->addTab('Hodnotenia', 'Hodnotenia/Priemery',
+            new HodnoteniaCallback($hodnoteniaScreen));
 
-		DisplayManager::addContent($zapisneListyCollapsible->getHtml());
-		
-		
-		$terminyHodnotenia = new
-			AIS2TerminyHodnoteniaScreen(
-          $trace,
-          $simpleConnection,
-          $adminStudia->getIdZapisnyList($trace, Input::get('list')),
-					$adminStudia->getIdStudium($trace, Input::get('list')));
-		
-		if (Input::get('tab') === null) Input::set('tab', 'TerminyHodnotenia');
-		$tabs = new TabManager('tab', array('studium'=>Input::get('studium'),
-					'list'=>Input::get('list')));
-		// FIXME: chceme to nejak refaktorovat, aby sme nevytvarali zbytocne
-		// objekty, ktore v konstruktore robia requesty
-		$hodnoteniaScreen = new AIS2HodnoteniaPriemeryScreen(
-          $trace, $simpleConnection,
-					$adminStudia->getIdZapisnyList($trace,
-            Input::get('list')));
-		$tabs->addTab('TerminyHodnotenia', 'Moje skúšky',
-					new MojeTerminyHodnoteniaCallback($trace->addChild("terminy hodnotenia callback"), $terminyHodnotenia, $hodnoteniaScreen));
-		$tabs->addTab('ZapisSkusok', 'Prihlásenie na skúšky',
-					new ZoznamTerminovCallback($terminyHodnotenia, $hodnoteniaScreen));
-		$tabs->addTab('ZapisnyList', 'Zápisný list',
-					new ZapisanePredmetyCallback($terminyHodnotenia));
-		$tabs->addTab('Hodnotenia', 'Hodnotenia/Priemery',
-				new HodnoteniaCallback($hodnoteniaScreen));
+        $tabs->setActive(Input::get('tab'));
+        DisplayManager::addContent($tabs->getHtml());
+        ;
+        $version = '<div>Fajr verzia '.hescape(Version::getVersionString()).'</div>';
+        DisplayManager::addContent($version);
+        $statistics = "<div> Fajr made ".$statsConnection->getTotalCount().
+                " requests and downloaded ".$rawStatsConnection->getTotalSize().
+                " bytes (".$statsConnection->getTotalSize().
+                " bytes uncompressed) of data from AIS2 in ".
+                sprintf("%.3f", $statsConnection->getTotalTime()).
+                " seconds. It took ".sprintf("%.3f", $timer->getElapsedTime()).
+                " seconds to generate this page.</div>";
+        DisplayManager::addContent($statistics);
+      }
+      else
+      {
+        DisplayManager::addContent('loginBox', true);	
+        DisplayManager::addContent('warnings', true);
+        DisplayManager::addContent('terms', true);
+        DisplayManager::addContent('credits', true);
+        $version = "<div class='version prepend-1 span-21 last increase-line-height'>\n<strong>Verzia fajru:</strong> \n";
+        $version .= hescape(Version::getVersionString());
+        $version .= '</div>';
+        DisplayManager::addContent($version);
+        DisplayManager::addContent(Version::getChangelog(), false);
+      }
+    }
+    catch (AIS2LoginException $e) {
+      if ($connection) FajrUtils::logout($connection);
+      DisplayManager::addException($e);
+    }
+    catch (Exception $e)
+    {
+      DisplayManager::addException($e);
+    }
 
-		$tabs->setActive(Input::get('tab'));
-		DisplayManager::addContent($tabs->getHtml());
-		;
-    $version = '<div>Fajr verzia '.hescape(Version::getVersionString()).'</div>';
-    DisplayManager::addContent($version);
-		$statistics = "<div> Fajr made ".$statsConnection->getTotalCount().
-						" requests and downloaded ".$rawStatsConnection->getTotalSize().
-						" bytes (".$statsConnection->getTotalSize().
-						" bytes uncompressed) of data from AIS2 in ".
-						sprintf("%.3f", $statsConnection->getTotalTime()).
-						" seconds. It took ".sprintf("%.3f", $timer->getElapsedTime()).
-						" seconds to generate this page.</div>";
-		DisplayManager::addContent($statistics);
-	}
-	else
-	{
-		DisplayManager::addContent('loginBox', true);	
-		DisplayManager::addContent('warnings', true);
-		DisplayManager::addContent('terms', true);
-		DisplayManager::addContent('credits', true);
-    $version = "<div class='version prepend-1 span-21 last increase-line-height'>\n<strong>Verzia fajru:</strong> \n";
-    $version .= hescape(Version::getVersionString());
-    $version .= '</div>';
-    DisplayManager::addContent($version);
-		DisplayManager::addContent(Version::getChangelog(), false);
-	}
-}
-catch (AIS2LoginException $e) {
-	if ($connection) FajrUtils::logout($connection);
-	DisplayManager::addException($e);
-}
-catch (Exception $e)
-{
-	DisplayManager::addException($e);
-}
+    DisplayManager::setBase(hescape(FajrUtils::basePath()));
 
-DisplayManager::setBase(hescape(FajrUtils::basePath()));
+    $trace->tlog("everything done, generating html");
 
-$trace->tlog("everything done, generating html");
-
-if (FajrConfig::get('Debug.Trace')===true) {
-  DisplayManager::addContent('<div class="span-24">' . $trace->getHtml() . '</div>');
-}
-echo DisplayManager::display();
-
-
+    if (FajrConfig::get('Debug.Trace')===true) {
+      DisplayManager::addContent('<div class="span-24">' . $trace->getHtml() . '</div>');
+    }
+    echo DisplayManager::display();
   }
 
   public function main()
