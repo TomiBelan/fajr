5e4f9f63d25050763b9b79c6a3027942284a182c
Peter Perešíni <ppershing@gmail.com>
2010-08-15 09:48:24 +0000

    Test ku GzipDecompressingConnection
    git-svn-id: https://fajr.googlecode.com/svn/branches/ppershing_trace_devel@240 e29e1642-1b52-11df-8da0-9131547f57cf

---
+++ libfajr/connection/GzipDecompressingConnectionTest.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ *
+ * @package libfajr
+ * @subpackage Tests
+ * @author Peter Peresini <ppershing+fajr@gmail.com>
+ */
+
+/**
+ * @ignore
+ */
+require_once 'test_include.php';
+
+use fajr\libfajr\connection\HttpConnection;
+use fajr\libfajr\connection\GzipDecompressingConnection;
+use fajr\libfajr\NullTrace;
+/**
+ * @ignore
+ */
+class GzipDecompressingConnectionTest extends PHPUnit_Framework_TestCase
+{
+  private function newConnection() {
+    return $this->getMock('fajr\libfajr\connection\HttpConnection',
+                          array('get', 'post', 'addCookie', 'clearCookies'));
+  }
+
+  public function testDecompress()
+  {
+    $response = file_get_contents(__DIR__.'/testdata/fmph.uniba.sk.gzip.dat');
+    $mockConnection = $this->newConnection();
+    $decompressingConnection =
+        new GzipDecompressingConnection($mockConnection, '/tmp');
+
+    $mockConnection->expects($this->once())
+                   ->method('get')
+                   ->will($this->returnValue($response));
+    $mockConnection->expects($this->once())
+                   ->method('post')
+                   ->will($this->returnValue($response));
+
+    $response = $decompressingConnection->get(new NullTrace, 'fmph.uniba.sk');
+    $this->assertRegExp("@<title>Fakulta matematiky, fyziky a informatiky</title>@",
+                        $response);
+
+    $response = $decompressingConnection->post(new NullTrace, 'fmph.uniba.sk', array());
+    $this->assertRegExp("@<title>Fakulta matematiky, fyziky a informatiky</title>@",
+                        $response);
+  }
+
+  public function testNoDecompress()
+  {
+    $response = file_get_contents(__DIR__.'/testdata/fmph.uniba.sk.zip.dat');
+    $mockConnection = $this->newConnection();
+    $decompressingConnection =
+        new GzipDecompressingConnection($mockConnection, '/tmp');
+
+    $mockConnection->expects($this->once())
+                   ->method('get')
+                   ->will($this->returnValue($response));
+    $mockConnection->expects($this->once())
+                   ->method('post')
+                   ->will($this->returnValue($response));
+
+    $response = $decompressingConnection->get(new NullTrace, 'fmph.uniba.sk');
+    $this->assertNotRegExp("@<title>Fakulta matematiky, fyziky a informatiky</title>@",
+                        $response);
+    $this->assertEquals('PK', substr($response, 0, 2));
+
+    $response = $decompressingConnection->post(new NullTrace, 'fmph.uniba.sk', null);
+    $this->assertNotRegExp("@<title>Fakulta matematiky, fyziky a informatiky</title>@",
+                        $response);
+    $this->assertEquals('PK', substr($response, 0, 2));
+
+    // We cannot use $mockConnection for returning more values of same function call
+    // @see bug http://www.phpunit.de/ticket/850
+    $mockConnection = $this->newConnection();
+    $decompressingConnection =
+        new GzipDecompressingConnection($mockConnection, '/tmp');
+    $mockConnection->expects($this->once())
+                   ->method('get')
+                   ->will($this->returnValue('Some response'));
+    $response = $decompressingConnection->get(new NullTrace, 'url');
+    $this->assertEquals('Some response', $response);
+  }
+
+
+}
+
+
