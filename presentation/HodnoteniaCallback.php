621cc429453727f1b2888600a48ba717f0c0b68d
Peter Perešíni <ppershing@gmail.com>
2010-08-21 21:54:28 +0000

    tabs->spaces presentation/*
    git-svn-id: https://fajr.googlecode.com/svn/branches/ppershing_trace_devel@302 e29e1642-1b52-11df-8da0-9131547f57cf

--- presentation/HodnoteniaCallback.php
+++ presentation/HodnoteniaCallback.php
@@ -4,56 +4,56 @@ use fajr\libfajr\base\Trace;
 use fajr\libfajr\window\VSES017_administracia_studia as VSES017;
 
 class HodnoteniaCallback implements Renderable {
-	private $app;
-	
-	public function __construct(Trace $trace, VSES017\HodnoteniaPriemeryScreen $app) {
-		$this->app = $app;
+  private $app;
+  
+  public function __construct(Trace $trace, VSES017\HodnoteniaPriemeryScreen $app) {
+    $this->app = $app;
     $this->trace = $trace;
-	}
-	
-	public function getHtml() {
+  }
+  
+  public function getHtml() {
     $trace = $this->trace->addChild("HodnoteniaCallback");
-		$hodnotenia = $this->app->getHodnotenia($trace);
-		$hodnoteniaTable = new Table(TableDefinitions::hodnotenia());
-		$priemeryCalculator = new PriemeryCalculator();
+    $hodnotenia = $this->app->getHodnotenia($trace);
+    $hodnoteniaTable = new Table(TableDefinitions::hodnotenia());
+    $priemeryCalculator = new PriemeryCalculator();
 
-		foreach(Sorter::sort($hodnotenia->getData(),
-					array("semester"=>-1, "nazov"=>1)) as $row) {
-			if ($row['semester']=='L') {
-				$class='leto';
-				$priemeryCalculator->add(PriemeryCalculator::SEMESTER_LETNY,
-					$row['znamka'], $row['kredit']);
-			}
-			else {
-				$class='zima';
-				$priemeryCalculator->add(PriemeryCalculator::SEMESTER_ZIMNY,
-					$row['znamka'], $row['kredit']);
-			}
-			$hodnoteniaTable->addRow($row, array('class'=>$class));
-		}
+    foreach(Sorter::sort($hodnotenia->getData(),
+          array("semester"=>-1, "nazov"=>1)) as $row) {
+      if ($row['semester']=='L') {
+        $class='leto';
+        $priemeryCalculator->add(PriemeryCalculator::SEMESTER_LETNY,
+          $row['znamka'], $row['kredit']);
+      }
+      else {
+        $class='zima';
+        $priemeryCalculator->add(PriemeryCalculator::SEMESTER_ZIMNY,
+          $row['znamka'], $row['kredit']);
+      }
+      $hodnoteniaTable->addRow($row, array('class'=>$class));
+    }
 
 
-		$hodnoteniaCollapsible = new Collapsible(new HtmlHeader('Hodnotenia'), $hodnoteniaTable);
-		
-		$priemery = $this->app->getPriemery($trace);
-		$priemeryTable = new Table(TableDefinitions::priemery());
-		$priemeryTable->addRows($priemery->getData());
+    $hodnoteniaCollapsible = new Collapsible(new HtmlHeader('Hodnotenia'), $hodnoteniaTable);
+    
+    $priemery = $this->app->getPriemery($trace);
+    $priemeryTable = new Table(TableDefinitions::priemery());
+    $priemeryTable->addRows($priemery->getData());
 
-		$priemeryContainer = new Container();
-		$priemeryContainer->addChild(new Label('Nasledovné priemery sú prebraté z AISu, čiže to (ne)funguje presne rovnako:'));
-		$priemeryContainer->addChild($priemeryTable);
+    $priemeryContainer = new Container();
+    $priemeryContainer->addChild(new Label('Nasledovné priemery sú prebraté z AISu, čiže to (ne)funguje presne rovnako:'));
+    $priemeryContainer->addChild($priemeryTable);
 
-		if ($priemeryCalculator->hasPriemer()) {
-			$priemeryFajrText = '<p><br />Nasledovné vážené študijné priemery sú počítané Fajrom priebežne z tabuľky Hodnotenia, <strong>preto nemôžu byť považované ako oficiálne</strong>:<br /><br />';
-			$priemeryFajrText .= $priemeryCalculator->getHtml();
-			$priemeryFajrText .= '</p>';
+    if ($priemeryCalculator->hasPriemer()) {
+      $priemeryFajrText = '<p><br />Nasledovné vážené študijné priemery sú počítané Fajrom priebežne z tabuľky Hodnotenia, <strong>preto nemôžu byť považované ako oficiálne</strong>:<br /><br />';
+      $priemeryFajrText .= $priemeryCalculator->getHtml();
+      $priemeryFajrText .= '</p>';
 
-			$priemeryContainer->addChild(new Label($priemeryFajrText));
-		}
-		
+      $priemeryContainer->addChild(new Label($priemeryFajrText));
+    }
+    
 
-		$priemeryCollapsible = new Collapsible(new HtmlHeader('Priemery'), $priemeryContainer);
-		
-		return $hodnoteniaCollapsible->getHtml().$priemeryCollapsible->getHtml();
-	}
+    $priemeryCollapsible = new Collapsible(new HtmlHeader('Priemery'), $priemeryContainer);
+    
+    return $hodnoteniaCollapsible->getHtml().$priemeryCollapsible->getHtml();
+  }
 }
--- presentation/ZapisanePredmetyCallback.php
+++ presentation/ZapisanePredmetyCallback.php
@@ -3,27 +3,27 @@
 use fajr\libfajr\base\Trace;
 
 class ZapisanePredmetyCallback implements Renderable {
-	private $skusky;
-	
-	public function __construct(Trace $trace, $skusky) {
+  private $skusky;
+  
+  public function __construct(Trace $trace, $skusky) {
     $this->trace = $trace;
-		$this->skusky = $skusky;
-	}
-	
-	public function getHtml() {
+    $this->skusky = $skusky;
+  }
+  
+  public function getHtml() {
     $trace = $this->trace->addChild("ZapisanePredmetyCallback");;
-		$predmetyZapisnehoListu = $this->skusky->getPredmetyZapisnehoListu($trace);
-		$predmetyZapisnehoListuTable = new
-			Table(TableDefinitions::predmetyZapisnehoListu());
-		$predmetyZapisnehoListuCollapsible = new Collapsible(new HtmlHeader('Predmety zápisného listu'),
-			$predmetyZapisnehoListuTable);
-		$kreditovCelkomLeto = 0;
+    $predmetyZapisnehoListu = $this->skusky->getPredmetyZapisnehoListu($trace);
+    $predmetyZapisnehoListuTable = new
+      Table(TableDefinitions::predmetyZapisnehoListu());
+    $predmetyZapisnehoListuCollapsible = new Collapsible(new HtmlHeader('Predmety zápisného listu'),
+      $predmetyZapisnehoListuTable);
+    $kreditovCelkomLeto = 0;
     $kreditovCelkomZima = 0;
     $pocetPredmetovLeto = 0;
     $pocetPredmetovZima = 0;
-		foreach (Sorter::sort($predmetyZapisnehoListu->getData(),
-					array("kodSemester"=>-1, "nazov"=>1)) as $row) {
-			if ($row['kodSemester']=='L') {
+    foreach (Sorter::sort($predmetyZapisnehoListu->getData(),
+          array("kodSemester"=>-1, "nazov"=>1)) as $row) {
+      if ($row['kodSemester']=='L') {
         $pocetPredmetovLeto += 1;
         $kreditovCelkomLeto += $row['kredit'];
         $class='leto';
@@ -33,9 +33,9 @@ class ZapisanePredmetyCallback implements Renderable {
         $kreditovCelkomZima += $row['kredit'];
         $class='zima';
       }
-			$predmetyZapisnehoListuTable->addRow($row, array('class'=>$class));
-			
-		}
+      $predmetyZapisnehoListuTable->addRow($row, array('class'=>$class));
+      
+    }
 
     $pocetPredmetovText = 'Celkom ';
     $pocetPredmetovText .= FajrUtils::formatPlural($pocetPredmetovLeto+$pocetPredmetovZima,
@@ -49,10 +49,10 @@ class ZapisanePredmetyCallback implements Renderable {
       $kreditovCelkomText .= sprintf(' (%d+%d)', $kreditovCelkomZima, $kreditovCelkomLeto);
     }
 
-		$predmetyZapisnehoListuTable->addFooter(array('nazov'=>$pocetPredmetovText,'kredit'=>$kreditovCelkomText), array());
-		$predmetyZapisnehoListuTable->setUrlParams(array('studium' =>
-					Input::get('studium'), 'list' => Input::get('list')));
-		
-		return $predmetyZapisnehoListuTable->getHtml();
-	}
+    $predmetyZapisnehoListuTable->addFooter(array('nazov'=>$pocetPredmetovText,'kredit'=>$kreditovCelkomText), array());
+    $predmetyZapisnehoListuTable->setUrlParams(array('studium' =>
+          Input::get('studium'), 'list' => Input::get('list')));
+    
+    return $predmetyZapisnehoListuTable->getHtml();
+  }
 }
