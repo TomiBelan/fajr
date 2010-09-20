72029e7103be76f14b26eec89ec33bb00774d7ee
Peter Perešíni <ppershing@gmail.com>
2010-09-20 19:48:53 +0000

    Upgradnuta verzia dependency grafu. Pridany blacklist na najfrekventovanejsie classy a taktiez pridane farby. Review URL: http://codereview.appspot.com/2163043
    git-svn-id: https://fajr.googlecode.com/svn/trunk@384 e29e1642-1b52-11df-8da0-9131547f57cf

--- scripts/phpdoc/dependency_graph.sh
+++ scripts/phpdoc/dependency_graph.sh
@@ -2,22 +2,34 @@
 REPORT=`readlink -f $1/report`;
 CLUSTER_PY=`readlink -f $(dirname $0)`/dependency_clusters.py;
 cd $1
-ZOZNAM=$(find * | grep '\.php' | grep -v '\.svn' | grep -v 'report' | \
+ZOZNAM=$(find src | grep '\.php' | grep -v '\.svn' | grep -v 'report' | \
     grep -v '\.swp' | LC_ALL='C' grep '[A-Z]' | grep -v 'Test' | \
     grep -v 'scripts' )
 
 
 OUT=$REPORT/class_dependency_graph.dot;
 
+# classy, ktore su velmi pouzivane nechceme zobrazovat
+# "Fajr" nie je pouzivana classa, ale poparsuje sa z @package
+BLACKLIST="Fajr Trace"
+
 echo "
 digraph g {
-concentrate=true;
-rankdir=\"LR\";
-clusterrank=\"local\";
-ranksep=\"3.0\";
-nodesep=\"0.5\";
-node[shape=box, fontsize=14, fillcolor=darkolivegreen1, style=filled];
+  concentrate=false;
+  rankdir=\"LR\";
+  clusterrank=\"local\";
+  ranksep=\"3.0\";
+  nodesep=\"0.5\";
+  node[shape=box, fontsize=14, fillcolor=darkolivegreen1, style=filled];
 " > $OUT;
+
+for class in $BLACKLIST; do
+  echo "  $class [fillcolor=gray];" >> $OUT;
+done
+
+function getcolor {
+  echo "$@" | sha1sum | sed 's/\(......\).*/\1/'
+}
 
 echo $ZOZNAM | $CLUSTER_PY >> $OUT;
 for file in $ZOZNAM; do
@@ -25,8 +37,14 @@ for file in $ZOZNAM; do
   VYSKYTY=`grep -l "[^a-zA-Z]$CLASS[^a-zA-Z]" $ZOZNAM`;
   for vyskyt in $VYSKYTY; do
     CLASS2=`basename $vyskyt | sed 's/\.php//'`
-    if [ $CLASS != $CLASS2 ] ; then
-      echo "$CLASS2 -> $CLASS;" >> $OUT
+    OK="true"
+    for blacklisted in $BLACKLIST; do
+      [ $CLASS != $blacklisted ] || OK="false"
+    done
+    [ $CLASS != $CLASS2 ] || OK="false";
+    if [ $OK == "true" ] ; then
+      col=`getcolor $CLASS`
+      echo "  $CLASS2 -> $CLASS [color=\"#$col\"];" >> $OUT
     fi
   done
 done
