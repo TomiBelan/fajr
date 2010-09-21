c6328f09614984d0a0e2ff4ad56e735559f7c198
Peter Perešíni <ppershing@gmail.com>
2010-09-21 11:19:44 +0000

    Pri coding standarde ignoruj third_party. Review URL: http://codereview.appspot.com/2239041
    git-svn-id: https://fajr.googlecode.com/svn/trunk@385 e29e1642-1b52-11df-8da0-9131547f57cf

--- scripts/make_coding_standard.sh
+++ scripts/make_coding_standard.sh
@@ -16,11 +16,13 @@ if [ $? -eq 0 ] ; then
     echo -n "Built on " >> $FILE;
     date >> $FILE;
     echo -e "\n" >> $FILE;
-    phpcs --standard="$CODING_STANDARD" . --report=full --ignore=report | tr '<>' '()' >> $FILE;
+    phpcs . --standard="$CODING_STANDARD" --report=full \
+        --ignore=report,third_party | tr '<>' '()' >> $FILE;
     echo -e "\n\n\n++++++++++++++++++++++++++++++++++++++++++++++++++\n\n\n" >> $FILE;
 
     echo "Second pass (statistics)"
-    phpcs --standard="$CODING_STANDARD" . --report=summary --ignore=report | tr '<>' '()' >> $FILE;
+    phpcs . --standard="$CODING_STANDARD" --report=summary \
+        --ignore=report,third_party | tr '<>' '()' >> $FILE;
     echo "</pre></html>" >> $FILE;
 
     echo "All done";
