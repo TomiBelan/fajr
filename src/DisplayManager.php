a0448137ea45d359ab68932f601d7e60f72f65d4
Martin Sucha <anty.sk@gmail.com>
2010-09-26 19:15:33 +0000

    Presunute generovanie Google Analytics snippetu do hlavneho layout
    git-svn-id: https://fajr.googlecode.com/svn/branches/ui_redesign@404 e29e1642-1b52-11df-8da0-9131547f57cf

--- src/DisplayManager.php
+++ src/DisplayManager.php
@@ -162,28 +162,10 @@ Prihlásením do systému Fajr súhlasíte s
       'legacy_content'=>$html,
       'base'=>$this->base,
       'debug_banner'=>FajrConfig::get('Debug.Banner'),
+      'google_analytics'=>FajrConfig::get('GoogleAnalytics.Account'),
       ));
     
     return $output;
-  }
-
-  protected function googleAnalytics()
-  {
-    $account = FajrConfig::get('GoogleAnalytics.Account');
-    if ($account === null) return '';
-    return '<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push([\'_setAccount\', \''.$account.'\']);
-  _gaq.push([\'_trackPageview\']);
-
-  (function() {
-    var ga = document.createElement(\'script\'); ga.type = \'text/javascript\'; ga.async = true;
-    ga.src = (\'https:\' == document.location.protocol ? \'https://ssl\' : \'http://www\') + \'.google-analytics.com/ga.js\';
-    var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>';
   }
 
   public static function getUniqueHTMLId($idType = 'id')
