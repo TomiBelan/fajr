430388ea38fcad83de3c27d8e173809f4b34c729
Peter Perešíni <ppershing@gmail.com>
2010-12-12 20:35:53 +0000

    Drobny fix aby DisableEvilCallsObject vypisoval spravnu classu.
    git-svn-id: https://fajr.googlecode.com/svn/trunk@518 e29e1642-1b52-11df-8da0-9131547f57cf

--- src/libfajr/base/DisableEvilCallsObject.php
+++ src/libfajr/base/DisableEvilCallsObject.php
@@ -18,42 +18,42 @@ class DisableEvilCallsObject
   public static function __callStatic($name, $arguments)
   {
     throw new Exception("Evil __callStatic() in "
-        . __CLASS__ . " --> $name");
+        . get_called_class() . " --> $name");
   }
 
   public static function __invoke($name, $arguments)
   {
     throw new Exception("Evil __invoke() in "
-        . __CLASS__ . " --> $name");
+        . get_called_class() . " --> $name");
   }
 
   public function __call($name, $arguments)
   {
     throw new Exception("Evil __call() in "
-        . __CLASS__ . " --> $name");
+        . get_called_class() . " --> $name");
   }
 
   public function __set($name, $arguments)
   {
     throw new Exception("Evil __set() in "
-        . __CLASS__ . " --> $name");
+        . get_called_class() . " --> $name");
   }
 
   public function __get($name)
   {
     throw new Exception("Evil __get() in "
-        . __CLASS__ . " --> $name");
+        . get_called_class() . " --> $name");
   }
 
   public function __isset($name)
   {
     throw new Exception("Evil __isset() in "
-        . __CLASS__ . " --> $name");
+        . get_called_class() . " --> $name");
   }
 
   public function __unset($name)
   {
     throw new Exception("Evil __unset() in "
-        . __CLASS__ . " --> $name");
+        . get_called_class() . " --> $name");
   }
 }
