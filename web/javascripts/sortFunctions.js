e9c3b413346bf658c35822d9075a9fdf43a3a44d
Martin Sucha <anty.sk@gmail.com>
2010-12-19 10:13:51 +0000

    Pridane triedy pre stlpce v tabulkach, aby fungovalo spravne triedenie. Fixes Issue 20 Review URL: http://codereview.appspot.com/3767041
    git-svn-id: https://fajr.googlecode.com/svn/trunk@560 e29e1642-1b52-11df-8da0-9131547f57cf

---
+++ web/javascripts/sortFunctions.js
@@ -0,0 +1,34 @@
+// Copyright (c) 2010 The Fajr authors (see AUTHORS).
+// Use of this source code is governed by a MIT license that can be
+// found in the LICENSE file in the project root directory.
+
+/**
+ * Dopln nuly na zaciatok retazca, az do dlzky n
+ */
+function pad0(s, n)
+{
+  while (s.length < n) {
+    s = '0' + s;
+  }
+  return s;
+}
+
+function sortPrihlasovanieOdhlasovaniePrepareData(tdNode, innerText)
+{
+  var m = innerText.match(/do (\d{1,2})\.(\d{1,2})\.(\d{4}) (\d{1,2}):(\d{2})$/);
+  if (!m) {
+    return -1;
+  }
+  // Vratime string tvaru YYYYMMDDhhmm
+  return pad0(m[3], 4) + pad0(m[2], 2) + pad0(m[1], 2) +
+         pad0(m[4], 2) + pad0(m[5], 2);
+}
+
+function sortPrihlasovanieOdhlasovanie(a, b)
+{
+  var aa = a[fdTableSort.pos];
+  var bb = b[fdTableSort.pos];
+  if(aa == bb) return 0;
+  if(aa < bb)  return -1;
+  return 1;
+}
