f751b5749d92919796265e2758f220b45eab59a4
Peter Perešíni <ppershing@gmail.com>
2010-12-20 12:24:02 +0000

    Pridane dalsie regresne definicie tabuliek. Review URL: http://codereview.appspot.com/3745043
    git-svn-id: https://fajr.googlecode.com/svn/trunk@576 e29e1642-1b52-11df-8da0-9131547f57cf

---
+++ src/regression/TerminyKPredmetuRegression.php
@@ -0,0 +1,24 @@
+<?php
+namespace fajr\regression;
+
+class TerminyKPredmetuRegression
+{
+  public static function get() {
+    return
+      array (
+        0 => 'kodFaza',
+        1 => 'dat',
+        2 => 'cas',
+        3 => 'miestnosti',
+        4 => 'pocetPrihlasenych',
+        5 => 'maxPocet',
+        6 => 'pocetHodn',
+        7 => 'hodnotiaci',
+        8 => 'prihlasovanie',
+        9 => 'odhlasovanie',
+        10 => 'poznamka',
+        11 => 'zaevidoval',
+        12 => 'moznostPrihlasenia',
+      );
+  }
+}
---
+++ src/regression/ZapisanePredmetyRegression.php
@@ -0,0 +1,22 @@
+<?php
+namespace fajr\regression;
+
+class ZapisanePredmetyRegression
+{
+  public static function get()
+  {
+    return
+      array (
+          0 => 'kodCastStPlanu',
+          1 => 'kodTypVyucby',
+          2 => 'skratka',
+          3 => 'nazov',
+          4 => 'kredit',
+          5 => 'kodSemester',
+          6 => 'kodSposUkon',
+          7 => 'pocetTerminov',
+          8 => 'pocetAktualnychTerminov',
+          9 => 'aktualnost',
+          );
+  }
+}
