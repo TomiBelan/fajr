40c62734ca059c116a4fcff9c23626b05b67a618
Peter Perešíni <ppershing@gmail.com>
2010-12-23 21:54:56 +0000

    Pridany missing test. Review URL: http://codereview.appspot.com/3764043
    git-svn-id: https://fajr.googlecode.com/svn/trunk@608 e29e1642-1b52-11df-8da0-9131547f57cf

--- tests/libfajr/login/CosignAbstractLoginTest.php
+++ tests/libfajr/login/CosignAbstractLoginTest.php
@@ -4,17 +4,22 @@
 // found in the LICENSE file in the project root directory.
 
 /**
- * This file contains tests for Validator class
+ * This file contains tests for CosignAbstractLogin class
  *
- * @package Fajr
- * @subpackage Tests
- * @author Peter Perešíni <ppershing+fajr@gmail.com>
+ * @package    Fajr
+ * @subpackage Libfajr__Login
+ * @author     Peter Perešíni <ppershing+fajr@gmail.com>
  */
+
 namespace fajr\libfajr\login;
-use PHPUnit_Framework_TestCase;
-use fajr\libfajr\pub\exceptions\LoginException;
+
 use fajr\libfajr\login\CosignAbstractLogin;
 use fajr\libfajr\pub\connection\HttpConnection;
+use fajr\libfajr\pub\connection\AIS2ServerConnection;
+use fajr\libfajr\pub\connection\AIS2ServerUrlMap;
+use fajr\libfajr\pub\exceptions\LoginException;
+use PHPUnit_Framework_TestCase;
+
 /**
  * @ignore
  */
@@ -25,9 +30,35 @@ require_once 'test_include.php';
  */
 class CosignAbstractLoginTest extends PHPUnit_Framework_TestCase
 {
+  public function setUp()
+  {
+    $this->connection = $this->getMock('\fajr\libfajr\pub\connection\HttpConnection');
+    $this->serverConnection = new AIS2ServerConnection($this->connection,
+                                                       new AIS2ServerUrlMap("ais2.test"));
+  }
+
   public function testLogout()
   {
-    $this->markTestIncomplete();
+    $data = file_get_contents(__DIR__.'/testdata/cosignNotLogged.dat');
+    $this->connection->expects($this->once())
+                     ->method('post')
+                     ->with($this->anything(), $this->stringContains('logout'))
+                     ->will($this->returnValue($data));
+
+    $login = $this->getMockForAbstractClass('\fajr\libfajr\login\CosignAbstractLogin');
+    $login->logout($this->serverConnection);
+  }
+
+  public function testFailedLogout()
+  {
+    $this->setExpectedException('\Exception');
+    $this->connection->expects($this->once())
+                     ->method('post')
+                     ->with($this->anything(), $this->stringContains('logout'))
+                     ->will($this->returnValue('not valid logout'));
+
+    $login = $this->getMockForAbstractClass('\fajr\libfajr\login\CosignAbstractLogin');
+    $login->logout($this->serverConnection);
   }
 }
 
