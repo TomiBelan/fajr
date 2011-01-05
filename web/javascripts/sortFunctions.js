a8d56af8f9d13b2cb400d93db417eec9bd5d5b39
Martin Sucha <anty.sk@gmail.com>
2011-01-05 23:15:43 +0000

    Pridana triediaca funkcia pre datum a cas + pouzitie v zozname prihlasenych na termin. Fixes Issue 75 Review URL: http://codereview.appspot.com/3816047
    git-svn-id: https://fajr.googlecode.com/svn/trunk@633 e29e1642-1b52-11df-8da0-9131547f57cf

--- web/javascripts/sortFunctions.js
+++ web/javascripts/sortFunctions.js
@@ -32,3 +32,23 @@ function sortPrihlasovanieOdhlasovanie(a, b)
   if(aa < bb)  return -1;
   return 1;
 }
+
+function sortDatumCasPrepareData(tdNode, innerText)
+{
+  var m = innerText.match(/(\d{1,2})\.(\d{1,2})\.(\d{4}) (\d{1,2}):(\d{2}):(\d{2})$/);
+  if (!m) {
+    return -1;
+  }
+  // Vratime string tvaru YYYYMMDDhhmmss
+  return pad0(m[3], 4) + pad0(m[2], 2) + pad0(m[1], 2) +
+         pad0(m[4], 2) + pad0(m[5], 2) + pad0(m[6], 2);
+}
+
+function sortDatumCas(a, b)
+{
+  var aa = a[fdTableSort.pos];
+  var bb = b[fdTableSort.pos];
+  if(aa == bb) return 0;
+  if(aa < bb)  return -1;
+  return 1;
+}
