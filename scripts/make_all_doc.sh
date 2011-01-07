2c1923a77cbbbbc933584e0d6ca03c6261f919cb
Peter Perešíni <ppershing@gmail.com>
2011-01-07 22:27:05 +0000

    Generovanie dokumentacie aj s javascriptom. Review URL: http://codereview.appspot.com/3911041
    git-svn-id: https://fajr.googlecode.com/svn/trunk@645 e29e1642-1b52-11df-8da0-9131547f57cf

--- scripts/make_all_doc.sh
+++ scripts/make_all_doc.sh
@@ -3,6 +3,7 @@ SCRIPT_GRAPH_CLASS=`dirname $0`/phpdoc/class_graph.sh;
 SCRIPT_GRAPH_CLASS_DEPENDENCY=`dirname $0`/phpdoc/dependency_graph.sh;
 SCRIPT_GRAPH_DIR=`dirname $0`/phpdoc/directory_graph.sh;
 SCRIPT_GRAPH_PACKAGE=`dirname $0`/phpdoc/package_graph.sh;
+JSCRIPT=`dirname $0`/../third_party/jsdoc/jsdoc.pl
 
 BASE_PATH=`dirname $0`/..
 $SCRIPT $BASE_PATH "Fajr";
@@ -14,3 +15,5 @@ echo "Generating dir graph"
 $SCRIPT_GRAPH_DIR $BASE_PATH
 echo "Generating package graph"
 $SCRIPT_GRAPH_PACKAGE $BASE_PATH
+echo "Generating javascript documentation"
+$JSCRIPT --directory $BASE_PATH/report/jsdoc --recursive $BASE_PATH/web $BASE_PATH/tests/web
