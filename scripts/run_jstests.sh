c3b17090b515ae8d1ac1a9539da5f465fa206285
Peter Perešíni <ppershing@gmail.com>
2011-01-08 19:54:16 +0000

    Pridane ukladanie vysledkov jstestov do suboru. Review URL: http://codereview.appspot.com/3908042
    git-svn-id: https://fajr.googlecode.com/svn/trunk@654 e29e1642-1b52-11df-8da0-9131547f57cf

--- scripts/run_jstests.sh
+++ scripts/run_jstests.sh
@@ -6,7 +6,12 @@ SCRIPT_PATH=`dirname $0`
 SCRIPT_PATH=`readlink -f $SCRIPT_PATH`
 TEST_PATH=`readlink -f $SCRIPT_PATH/../tests`
 JSDRIVER_BIN=`readlink -f $SCRIPT_PATH/../third_party/jstestdriver/JsTestDriver-1.2.2.jar`
+REPORT_PATH=`readlink -f $SCRIPT_PATH/../report/tests/`
 
-cd $TEST_PATH && java -jar $JSDRIVER_BIN --verbose --tests all | \
-  sed -u -e 's/^\(\[PASSED\].*\)/\o033[0;32m\1\o033[0m/' |
-  sed -u -e 's/^\(\[FAILED\].*\)/\o033[0;31m\1\o033[0m/'
+mkdir -p $REPORT_PATH;
+
+cd $TEST_PATH && java -jar $JSDRIVER_BIN --verbose --tests all $@ | \
+  tee $REPORT_PATH/js_tests.txt | \
+  sed -u -e 's/^\(\[PASSED\].*\)/\o033[0;32m\1\o033[0m/' | \
+  sed -u -e 's/^\(\[FAILED\].*\)/\o033[0;31m\1\o033[0m/' | \
+  sed -u -e 's/^\(\[ERROR\].*\)/\o033[0;31m\1\o033[0m/'
