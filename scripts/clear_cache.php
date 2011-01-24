a88548b2761f4eebf0e155ecfa4ed97b10af8691
Peter Perešíni <ppershing@gmail.com>
2011-01-24 15:59:20 +0000

    Clear cache skript fixnuty na novy FajrConfig. Review URL: http://codereview.appspot.com/4095044
    git-svn-id: https://fajr.googlecode.com/svn/trunk@702 e29e1642-1b52-11df-8da0-9131547f57cf

--- scripts/clear_cache.php
+++ scripts/clear_cache.php
@@ -1,23 +1,25 @@
 #!/usr/bin/php
 <?php
-use fajr\config\FajrConfig;
+use fajr\config\FajrConfigLoader;
 
 // register our autoloader
 require_once (__DIR__ . '/../src/libfajr/libfajr.php');
 Loader::register();
 Loader::searchForClasses(__DIR__ . '/../src', true);
 
-if (!FajrConfig::isConfigured()) {
+if (!FajrConfigLoader::isConfigured()) {
   echo 'Chyba: Fajr nie je nakonfigurovany'. "\n";
   return;
 }
 
-if (!FajrConfig::get('Template.Cache')) {
+$config = FajrConfigLoader::getConfiguration();
+
+if (!$config->get('Template.Cache')) {
   echo 'Info: Template cache je vypnuta, nema zmysel ju mazat'. "\n";
   return;
 }
 
-$path = FajrConfig::getDirectory('Template.Cache.Path');
+$path = $config->getDirectory('Template.Cache.Path');
 echo 'Info: Template cache je ' . $path . "\n";
 
 foreach (new DirectoryIterator($path) as $fileInfo) {
