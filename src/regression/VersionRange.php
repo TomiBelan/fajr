3aa06158dd63c7f96288595ffdaf92b0ed8e9d98
Martin Sucha <anty.sk@gmail.com>
2011-02-05 11:55:03 +0000

    Zvysena podporovana verzia AIS-u na 2.3.21.26
    git-svn-id: https://fajr.googlecode.com/svn/trunk@731 e29e1642-1b52-11df-8da0-9131547f57cf

--- src/regression/VersionRange.php
+++ src/regression/VersionRange.php
@@ -1,5 +1,5 @@
 <?php
-// Copyright (c) 2010 The Fajr authors.
+// Copyright (c) 2010,2011 The Fajr authors.
 // Use of this source code is governed by a MIT license that can be
 // found in the LICENSE file in the project root directory.
 /**
@@ -44,7 +44,7 @@ class VersionRange
    */
   public static function getMaxVersion()
   {
-    return new AIS2Version(2, 3, 21, 23);
+    return new AIS2Version(2, 3, 21, 26);
   }
 
 }
