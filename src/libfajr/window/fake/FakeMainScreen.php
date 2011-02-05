a2a168143fca0c1ddc76a677241f0080914f1b93
Martin Sucha <anty.sk@gmail.com>
2011-02-05 20:10:10 +0000

    Mergnuty fix na fake verziu AIS-u aj do trunku
    git-svn-id: https://fajr.googlecode.com/svn/trunk@738 e29e1642-1b52-11df-8da0-9131547f57cf

--- src/libfajr/window/fake/FakeMainScreen.php
+++ src/libfajr/window/fake/FakeMainScreen.php
@@ -1,5 +1,5 @@
 <?php
-// Copyright (c) 2010 The Fajr authors.
+// Copyright (c) 2010,2011 The Fajr authors.
 // Use of this source code is governed by a MIT license that can be
 // found in the LICENSE file in the project root directory.
 
@@ -34,7 +34,7 @@ class FakeMainScreen implements AIS2MainScreen
   public function getAisVersion(Trace $trace)
   {
     $trace->tlog('retrieving ais version');
-    return new AIS2Version(2, 3, 19, 35);
+    return new AIS2Version(2, 3, 21, 26);
   }
 
   /**
