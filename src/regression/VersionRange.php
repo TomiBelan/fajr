a1c20a45800881aa324547504dc3dfd3ed101c71
Martin Sucha <anty.sk@gmail.com>
2011-04-05 13:04:16 +0000

    Mergnuta podpora novsieho AIS-u do stabilnej vetvy
    git-svn-id: https://fajr.googlecode.com/svn/branches/0.4.x@780 e29e1642-1b52-11df-8da0-9131547f57cf

--- src/regression/VersionRange.php
+++ src/regression/VersionRange.php
@@ -33,7 +33,7 @@ class VersionRange
    */
   public static function getMinVersion()
   {
-    return new AIS2Version(2, 3, 21, 20);
+    return new AIS2Version(2, 3, 22, 52);
   }
 
   /**
@@ -44,7 +44,7 @@ class VersionRange
    */
   public static function getMaxVersion()
   {
-    return new AIS2Version(2, 3, 21, 26);
+    return new AIS2Version(2, 3, 22, 52);
   }
 
 }
