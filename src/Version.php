4d5fa9760eb9e3a16fd368024f84aceedb930598
Martin Sucha <anty.sk@gmail.com>
2011-12-20 19:48:28 +0100

    Updatnute cislo verzie na 0.5.0

--- src/Version.php
+++ src/Version.php
@@ -23,7 +23,7 @@ namespace fajr;
 class Version
 {
   /** @var string current version number */
-  private static $version = '0.5.0alpha0';
+  private static $version = '0.5.0';
 
   /** @var array basic changelog information */
   private static $changelog = array (
@@ -43,6 +43,7 @@ class Version
       array('2011-01-06', '0.4.4', 'Ďaľšie drobné opravy, nové testy, demo režim'),
       array('2011-01-16', '0.4.5', 'Ďaľšie fixy, testy'),
       array('2011-01-21', '0.4.6', 'Opravy (povolenie prihlasovania na skúšky pri Fx a triedenie mien v zozname prihlásených na skúšku). Základ pre podporu viac vzhľadov'),
+      array('2011-12-20', '0.5.0', 'Refaktoring, vylepšený vzhľad, ...'),
     );
 
   /** @var int how many entries from changelog we want to show */
