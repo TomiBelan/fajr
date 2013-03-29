5aa858a44974acf544949d4a16b008a4696810e8
Dusan Plavak <dusan.plavak@gmail.com>
2013-03-29 16:55:36 +0100

    implementovane updateComponentFromResponse
    Cela implementacia prevazne spocivala v tom ze som zobral kod z AIS2TableParser a z AIS2HTMLParser trocha ho poupravoval...

--- libfajr/src/data/DataTable/DataTable.php
+++ libfajr/src/data/DataTable/DataTable.php
@@ -47,9 +47,10 @@ class DataTable implements ComponentInterface
   private $selectedRows = null;
 
   /**
-   * Create a Table and set its idName and definition
+   * Create a Table and set its dataViewName and definition
    *
    * @param string $dataViewName name of Table which we want to store here
+   * @param Window $parent window where this component is used
    * @param array(string) $definition name of columns which table must have
    *                                  if not defined, no control on that will
    *                                  be done during updating the table and 
@@ -58,7 +59,12 @@ class DataTable implements ComponentInterface
   public function __construct($dataViewName, $definition = null)
   {
     $this->dataViewName = $dataViewName;
-    $this->controlDefinition = $definition;
+    if($definition != null){
+      $this->controlDefinition = $definition;
+    }else{
+      $this->controlDefinition = array();
+    }
+    $this->selectedRows = array();
   }
 
   /**
@@ -68,6 +74,46 @@ class DataTable implements ComponentInterface
    */
   public function updateComponentFromResponse(DOMDocument $aisResponse)
   {
+    $element = $aisResponse->getElementById($dataViewName);
+    $dom = new DOMDocument();
+    $dom->appendChild($dom->importNode($element, true));
+    // ok, now we have restricted document
+
+    //informacia ci sa jedna o update, append...
+    $element2 = $dom->getElementById("dataTabBodies");
+    if ($element === null || $element2 === null) {
+      throw new ParseException("Problem parsing ais2 response: Element not found");
+    }
+    
+
+    //ak sa jedna len o scroll tak tam definicia tabulky nie je
+    if($element2->getAttribute("dataSendType") == "update"){
+      $this->definition = $this->getDefinitionFromDom($dom);
+      $control = array_diff($this->controlDefinition,$this->definition);
+      if($control != null){
+        throw new ParseException("Table definition isn`t a subset of controlDefinition");
+      }
+    }
+
+    $TData = $this->getTableDataFromDom($dom);
+    
+    // modifying data to better format
+    assert(is_array($TData));
+    $this->data = array();
+    foreach ($TData as $rowKey=>$tableRow) {
+      $myRow = array();
+      $myRow['index'] = $rowKey;
+      assert(count($this->definition) == count($tableRow));
+
+      foreach($tableRow as $key=>$value) {
+        assert(is_numeric($key));
+        assert(isset($this->definition[$key]));
+        $myRow[$this->definition[$key]] = $value;
+      }
+
+      $this->data[$rowKey] = $myRow;
+
+    }
   
   }
 
@@ -94,7 +140,7 @@ class DataTable implements ComponentInterface
   /**
    * Return one record from table
    *
-   * @param integer $index number of row, which we want to get
+   * @param string $index rowId of row, which we want to get
    * @returns array(string=>string) data in row $data[$index].
    */
   public function getRow($index)
@@ -115,7 +161,7 @@ class DataTable implements ComponentInterface
   /**
    * Add one row to selection
    *
-   * @param integer $index number of row, which we want to select
+   * @param string $index rowId of row, which we want to select
    */
   public function selectRow($index)
   {
@@ -125,7 +171,7 @@ class DataTable implements ComponentInterface
  /**
    * Select one record of table
    *
-   * @param integer $index number of row, which we want to select
+   * @param string $index rowId of row, which we want to select
    */
   public function selectSingleRow($index)
   {
@@ -140,5 +186,71 @@ class DataTable implements ComponentInterface
   public function clearSelection()
   {
     $this->selectedRows = array();
+  }
+
+  /**
+   * Get table definitions from DOMDocument
+   *
+   * @param $dom DOMDocument from ais2ResponseHtml
+   * @returns array(string) Definition of table
+   */
+  private function getDefinitionFromDom(DOMDocument $dom)
+  {
+    $element = $dom->getElementById('dataTabColGroup');
+    if ($element == null) {
+      throw new ParseException("Can't find table headers");
+    }
+    $list = $element->getElementsByTagName('col');
+    $columns = array();
+    foreach ($list as $node) {
+      assert($node->hasAttribute('shortname'));
+      $columns[] = $node->getAttribute('shortname');
+    }
+    return $columns;
+  }
+
+  /**
+   * Extract Table data from DOMDocument
+   *
+   * @param $dom DOMDocument part of ais2ResponseHTML which contain Table
+   * @returns array(string=>array(string)) Returns rows of Table, where index is rowId
+   */
+  private function getTableDataFromDom(DOMDocument $dom)
+  {
+    $Tdata = array();
+    $element = $dom->getElementById('dataTabBody0');
+    if ($element == null) {
+      throw new ParseException("Can't find table data");
+    }
+
+    foreach ($element->childNodes as $aisRow) {
+      assert($aisRow->tagName == "tr");
+      assert($aisRow->hasAttribute("id"));
+      assert($aisRow->hasChildNodes());
+      // TODO: asserty prerobit na exceptiony
+      $row = array();
+      $rowId = $aisRow->getAttribute("id");
+      $index = StrUtil::match('@^row_([0-9]+)$@', $rowId);
+      if ($index === false) {
+        throw new ParseException("Unexpected row id format");
+      }
+      foreach ($aisRow->childNodes as $ais_td) {
+        assert($ais_td->tagName == "td");
+        // get a text content of <td>
+        $value = $ais_td->textContent;
+        Preconditions::checkIsString($value);
+        // special fix for &nbsp;
+        $nbsp = chr(0xC2).chr(0xA0);
+        $value = str_replace($nbsp, ' ', $value);
+
+        if ($value == ' ') {
+          $row[] = '';
+        }
+        assert($value != ''); // probably there is some inner element which we don't know about
+        $row[] = $value;
+      }
+      $Tdata[$index] = $row;
+    }
+    return $Tdata;
   }
 }
